f4e9475f139d5aa6d752962e806a064fb8493989
tyl-brent <67525842+tyl-brent@users.noreply.github.com>
2020-09-25 20:51:08 -0500

    Add files via upload

---
+++ ui/mods/academia/maps.js
@@ -0,0 +1,21 @@
+cShareSystems.load_pas("Academia Teams", [
+    "coui://ui/mods/academia/systems/academioe_5v5.pas",
+	"coui://ui/mods/academia/systems/academius_5v5.pas",
+    "coui://ui/mods/academia/systems/floataurius_5v5 astroid 10 mins.pas",
+    "coui://ui/mods/academia/systems/floataurius_5v5.pas",
+    "coui://ui/mods/academia/systems/hilarioe_5v5 astroid 10 mins.pas",
+    "coui://ui/mods/academia/systems/hilarioe_5v5 no astroids.pas",
+    "coui://ui/mods/academia/systems/hilarioe_5v5.pas",
+	"coui://ui/mods/academia/systems/kaixo_6v6.pas",
+	"coui://ui/mods/academia/systems/pokeball_4v4.pas",
+	"coui://ui/mods/academia/systems/stirix_4v4.pas",
+	"coui://ui/mods/academia/systems/stirix-duo_4v4.pas",
+"coui://ui/mods/academia/systems/tianion_4v4.pas",
+"coui://ui/mods/academia/systems/xaxium-4_7v7.pas",
+"coui://ui/mods/academia/systems/xu'er-a.pas",
+"coui://ui/mods/academia/systems/xu'er-s.pas",
+"coui://ui/mods/academia/systems/zeptune-1_10v10.pas",
+"coui://ui/mods/academia/systems/zyion-alpha_8v8.pas",
+"coui://ui/mods/academia/systems/zypherium_8v8.pas",
+"coui://ui/mods/academia/systems/zypherium_duo 8v8.pas"
+]);
